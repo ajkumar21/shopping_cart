92bbba30c2bd82de25f9819cc86535c23044589a
AJ Kumar <ajanthan29894@gmail.com>
2020-03-17 15:26:04 +0000

    implemented tests for calculating savings and implemented the function to pass the tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import * as enzyme from 'enzyme';
 import { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { calculateCost } from './utils/utils';
+import { calculateCost, calculateSavings } from './utils/utils';
 
 const adapter = new Adapter();
 enzyme.configure({ adapter });
@@ -24,5 +24,53 @@ describe('helper functions', () => {
     const examplePrice = 2.0;
     const cost = calculateCost(exampleQuantity, examplePrice);
     expect(cost).to.eq(8);
+  });
+
+  it('should calculate savings for 1 coke', () => {
+    const itemName = 'Coke';
+    const exampleQuantity = 1;
+    const examplePrice = 0.7;
+    const savings = calculateSavings(itemName, exampleQuantity, examplePrice);
+    expect(savings).to.eq(0);
+  });
+
+  it('should calculate savings for 2 cokes', () => {
+    const itemName = 'Coke';
+    const exampleQuantity = 2;
+    const examplePrice = 0.7;
+    const savings = calculateSavings(itemName, exampleQuantity, examplePrice);
+    expect(savings).to.eq(0.4);
+  });
+
+  it('should calculate savings for 3 cokes', () => {
+    const itemName = 'Coke';
+    const exampleQuantity = 2;
+    const examplePrice = 0.7;
+    const savings = calculateSavings(itemName, exampleQuantity, examplePrice);
+    expect(savings).to.eq(0.4);
+  });
+
+  it('should calculate savings for 2 cans of beans', () => {
+    const itemName = 'Beans';
+    const exampleQuantity = 2;
+    const examplePrice = 0.5;
+    const savings = calculateSavings(itemName, exampleQuantity, examplePrice);
+    expect(savings).to.eq(0);
+  });
+
+  it('should calculate savings for 3 cans of beans', () => {
+    const itemName = 'Beans';
+    const exampleQuantity = 3;
+    const examplePrice = 0.5;
+    const savings = calculateSavings(itemName, exampleQuantity, examplePrice);
+    expect(savings).to.eq(0.5);
+  });
+
+  it('should calculate savings for 4 cans of beans', () => {
+    const itemName = 'Beans';
+    const exampleQuantity = 3;
+    const examplePrice = 0.5;
+    const savings = calculateSavings(itemName, exampleQuantity, examplePrice);
+    expect(savings).to.eq(0.5);
   });
 });
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,17 @@
 export function calculateCost(quantity, price) {
   return quantity * price;
 }
+
+export function calculateSavings(name, quantity, price) {
+  if (name === 'Coke') {
+    //2 for £1
+    //cart[name].price * 2 - 1 = saving after multibuy
+    const saving = Math.floor(quantity / 2) * (price * 2 - 1);
+    return parseFloat(saving.toFixed(2));
+  } else if (name === 'Beans') {
+    //3 for 2
+    const saving = Math.floor(quantity / 3) * price;
+    return parseFloat(saving.toFixed(2));
+  }
+  return 0;
+}
